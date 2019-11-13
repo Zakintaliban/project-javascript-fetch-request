a465518f516f9051de93c32de3f952652c22d055
zakintaliban <nukmans23@gmail.com>
2019-11-13 16:30:19 +0700

    v1.1

--- fetch.js
+++ fetch.js
@@ -1,30 +1,29 @@
 function getUserFollower() {
-  let div = document.createElement("div");
   let item = document.createElement("div");
   fetch("https://api.github.com/users/Zakintaliban/followers")
     .then(res => res.json())
     .then(follower => {
       follower.map(data => {
         const img = document.createElement("img");
-        let rowow = document.createElement("div");
+        let divider = document.createElement("div");
         let imgHolder = document.createElement("div");
-        let namename = document.createElement("a");
-        let infoinfo = document.createElement("p")
+        let nameLink = document.createElement("a");
+        let paragraphInfo = document.createElement("p");
 
         img.setAttribute("src", data.avatar_url);
         img.className = "img-fluid max-width:100%";
-        namename.innerText = data.login;
-        namename.href = data.html_url
-        rowow.className = "col-md-4";
+        nameLink.innerText = data.login;
+        nameLink.href = data.html_url;
+        divider.className = "col-md-4";
         imgHolder.className = "card mb-4 shadow-sm";
         item.className = "row";
-        infoinfo.innerText = data.id
+        paragraphInfo.innerText = data.id;
 
         imgHolder.append(img);
-        rowow.append(imgHolder);
-        item.append(rowow);
-        imgHolder.append(namename);
-        imgHolder.append(infoinfo)
+        divider.append(imgHolder);
+        item.append(divider);
+        imgHolder.append(nameLink);
+        imgHolder.append(paragraphInfo);
 
         document.getElementById("main").append(item);
       });
